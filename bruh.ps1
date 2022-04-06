fd314ad0278f5598031ef96960c36ed67124cc0c
Alvsch <94403567+Alvsch@users.noreply.github.com>
2022-04-06 10:07:07 +0200

    Update bruh.ps1

--- bruh.ps1
+++ bruh.ps1
@@ -1,5 +1,4 @@
-curl -Uri "https://www.myinstants.com/media/sounds/movie_1.mp3" -OutFile "c:\users\as17004\desktop\bruh.mp3"
-Add-Type -AssemblyName presentationCore
-$mediaPlayer = New-Object system.windows.media.mediaplayer
-$mediaPlayer.open('c:\users\as17004\desktop\bruh.mp3')
-$mediaPlayer.Play()
+cd "c:\users\as17004\desktop"
+
+Add-Type -AssemblyName presentationCore; $mediaPlayer = New-Object system.windows.media.mediaplayer; $mediaPlayer.open('c:\users\as17004\desktop\bruh.mp3'); $mediaPlayer.Play()
+pause
