143587b338fe5b5f3bdf2d5158c46a0eb6e5a022
Alvsch <94403567+Alvsch@users.noreply.github.com>
2022-04-06 09:53:07 +0200

    Update bruh.ps1

--- bruh.ps1
+++ bruh.ps1
@@ -1,5 +1,5 @@
-curl -Uri "https://www.myinstants.com/media/sounds/movie_1.mp3" -OutFile "bruh.mp3"
+curl -Uri "https://www.myinstants.com/media/sounds/movie_1.mp3" -OutFile "c:\users\as17004\desktop\bruh.mp3"
 Add-Type -AssemblyName presentationCore
 $mediaPlayer = New-Object system.windows.media.mediaplayer
-$mediaPlayer.open('C:\Users\as17004\Desktop\bruh.mp3')
+$mediaPlayer.open('c:\users\as17004\desktop\bruh.mp3')
 $mediaPlayer.Play()
